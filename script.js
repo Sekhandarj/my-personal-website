93d0dc89379540133d8ac06b45737a0da5882229
Sekhandar Jagdeo <Sekhandarj@gmail.com>
2023-09-05 15:05:09 -0400

    new update

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const h2 = document.createElement("h2")
-h2.textContent = "Destruction of Tyre";
-document.querySelector("body").appendChild(h2);
+document.addEventListener('DOMContentLoaded', () => {
+let form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_todo.value)
+    form.reset()
+})
+})
 
+function buildToDo(todo){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    p.textContent = `${todo} `
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector('#todo_container').appendChild(p)
+}
+
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
